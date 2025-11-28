5a54525145d279d57d6fb232aebeeb55e643e658
Higini <34925502+HiginiFernandez@users.noreply.github.com>
2025-11-28 18:41:55 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.py
@@ -0,0 +1,1 @@
+Higini.enviar_missatge()
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Higini_Proves.cercle(1))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Higini_Proves.cercle(0))
+})
